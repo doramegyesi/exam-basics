42776eeb5a900be48c9cbeaaacc05c6851650ec6
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-18 09:58:05 +0200

    basic plan for the solution

--- oddavg/odd_avg.py
+++ oddavg/odd_avg.py
@@ -1,3 +1,9 @@
 # Create a function called `odd_average` that takes a list of numbers as parameter
 # and returns the average value of the odd numbers in the list
 # Create basic unit tests for it with at least 3 different test cases
+
+def odd_average():
+    list_of_numbers = []
+    #needs a for loop
+    #choose odd numbers
+    #return sum(numbers)/len(numbers)
