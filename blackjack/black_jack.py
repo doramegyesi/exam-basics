634058c023378676ca3db184438b28f149ee3528
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-18 11:17:01 +0200

    constructor of Card class is created

--- blackjack/black_jack.py
+++ blackjack/black_jack.py
@@ -5,11 +5,25 @@
 # Jack Diamonds
 # Create a Deck class, that has a list of cards in it
 # Create a constructor that takes a whole number as parameter
-# The constructor should fill the list with the number of different cards using at least 4 different colors (except if the number given is smaller than four, than all cards should have different colors)
+# The constructor should fill the list with the number of different cards using at least 4 different colors
+#(except if the number given is smaller than four, than all cards should have different colors)
 # We should be able to shuffle the deck, which randomly orders the cards
 # We should be able to draw the top card which returns the drawn card and also removes it from the deck
 # Deck should be represented as string in this format:
 # 12 cards -  3 Clubs, 3 Diamonds, 3 Hearts, 3 Spades
+
+class Card():
+    def __init__(self, color = "", value = ""):
+        self.color = color
+        self.value = value
+        self.card = self.value + self.color
+
+class Deck():
+    #list of cards
+    #constructor as a whole nomber
+
+
+
 deck = Deck(12)
 print(deck)
 # Should print out:
