162fb6d2bfc6269b33f173d50a038c77676d3bc6
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-18 10:17:26 +0200

    refactored so it returs with a zero

--- oddavg/odd_avg.py
+++ oddavg/odd_avg.py
@@ -2,13 +2,14 @@
 # and returns the average value of the odd numbers in the list
 # Create basic unit tests for it with at least 3 different test cases
 
-#numbers = [1, 2, 3, 4, 5]
 
 def odd_average(numbers = []):
     list_of_numbers = []
+    average = 0
     for n in range(len(numbers)):
         if n % 2 != 0:
-            list_of_numbers.append(sum(n) / len(n))
-        return list_of_numbers
+            list_of_numbers.append(n)
+            average = str(sum(list_of_numbers) / len(list_of_numbers))
+        return average
 
 print(odd_average(numbers = [1,2,3]))
