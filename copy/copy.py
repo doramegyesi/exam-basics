697e178170b3e1a0ea0232538118b7b3e04e9c5e
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-18 11:00:56 +0200

    first steps getting nowhere

--- copy/copy.py
+++ copy/copy.py
@@ -6,3 +6,15 @@
 # No destination provided
 # When both arguments provided and the source is a file
 # Read all contents from it and write it to the destination
+
+def copy_a_file(filename, filename2):
+    text = open(filename, "w")
+    t = text.readlines()
+    text2 = open(filename2, "r")
+    t2 = text2.readlines()
+    text = text2
+    text.close()
+    text2.close()
+
+
+print(copy_a_file("new.txt", "old.txt"))
